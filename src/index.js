1de74c2326b5a5648d2ddf4da62c86d0f01623c5
Anand <anandof86@icloud.com>
2018-10-12 13:06:22 +0530

    Complete React & Redux Structure

--- src/index.js
+++ src/index.js
@@ -1,39 +1,78 @@
 import React from "react";
 import { render } from "react-dom";
 import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+import { Provider, connect } from 'react-redux';
 import "bootstrap/dist/css/bootstrap.css";
+
+/** Redux Layer Starts Here */
 import {ReviewReducer} from './reducers/ReviewReducer';
 import {INCREMENT_ACTION} from './action/reviewaction';
 
 //Create Store
 const appStore = createStore(ReviewReducer);
+/** Redux Layer Ends Here */
+
+
+/**
+ * Redux State to React Prop Mapper
+ * 
+ */
+
+const mapStatetoProps = like =>{
+    return {
+        value : like
+    }
+} 
+
+const mapDispatchToProps = dispatch =>{
+    return {
+        onIncrement: function() {
+            dispatch(INCREMENT_ACTION);
+        }
+    };
+}
+//Presentaion Layer
+
+const ReviewPresention = props =>(
+    <React.Fragment>
+        <h5 className="card-title">Review App</h5>
+        <p className="card-text">This will indicate total no of likes gained, based on user clicks</p>
+        <span className="card-link">Like : {props.value}</span>
+        <button type="button" onClick={() => props.onIncrement()} className="btn btn-success btn-sm card-link">Like Counter</button>
+    </React.Fragment>
+);
+
+
+//Container Component Layer
 
 class App extends React.Component {
-    state = {
-        like : 0
-    }
-
-    onIncrement = () =>{
-        appStore.dispatch(INCREMENT_ACTION) 
-        const like = appStore.getState();
-        this.setState({like});
-    }
-
+   
     render() {
       return (
             <div className="container">
-                <div className="card col-md-6">
-                    <div className="card-body">
-                        <h5 className="card-title">Review App</h5>
-                        <p className="card-text">This will indicate total no of likes gained, based on user clicks</p>
-                        <span className="card-link">Like : {this.state.like}</span>
-                        <button type="button" onClick={this.onIncrement} className="btn btn-success btn-sm card-link">Like Counter</button>
+                    <div className="card col-md-6">
+                        <div className="card-body">
+                            <ReviewPresention {...this.props}/>
+                        </div>
                     </div>
-                </div>
             </div>
         );
     }
-}
 
-render(<App />, document.getElementById("root"));
+    
+   
+}
+//Create a Create Container Component
+const ReviewContainer = connect(
+    mapStatetoProps,
+    mapDispatchToProps
+)(App);
+
+const MainApp = () => (
+    <Provider store={appStore}>
+      <ReviewContainer  />
+    </Provider>
+);
+  
+
+render(<MainApp />, document.getElementById("root"));
