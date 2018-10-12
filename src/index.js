51cd60b10969b14c5b03d027f9eabaa931116e17
Anand <anandof86@icloud.com>
2018-10-12 11:23:49 +0530

    Adding Redux & Reducer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import React from "react";
 import { render } from "react-dom";
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import "bootstrap/dist/css/bootstrap.css";
+/**
+ * Create a Reducer First
+ * -> Simple JS Function
+ * -> Reducer must take two arg (Initial State like react state variable declaration,action )
+ * -> State can be any premitive literals 
+ * -> reducer must return default state
+ * -> Create Store object
+ */
+//Reducer
+function ReviewReducer(like=0, action){
 
+    return like;
+}
+//Create Store
+const appStore = createStore(ReviewReducer);
+console.log(appStore)
+ 
 class App extends React.Component {
     render(){
         return(
